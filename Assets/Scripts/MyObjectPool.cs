e34ce37470f2ba3c1d3a73acbeef835519f795c5
jet1202 <jet86488@gmail.com>
2023-05-06 15:59:33 +0900

    ObjectPool追加1

---
+++ Assets/Scripts/MyObjectPool.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyObjectPool : MonoBehaviour
+{
+    private List<GameObject> _pool;
+    public GameObject prefab;
+}
