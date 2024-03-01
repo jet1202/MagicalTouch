6e21e0c3c95d469bfdefe00ddcb6035b6f061ce6
jet1202 <jet86488@gmail.com>
2024-03-01 23:06:05 +0900

    CategorySelectSceneのレイアウト

---
+++ Assets/Projects/Scripts/Category/CategoryDataFormat.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CategoryData
+{
+    public string[] tabs;
+    public Category[] item;
+}
+
+public class Category
+{
+    public string name;
+    public string sub;
+    public string division;
+    public int number;
+    public int tab;
+}
